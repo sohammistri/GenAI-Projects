3219f78458a43038c399302d11ad5ea33198fba0
sohammistri <soham07.mistri12@gmail.com>
2024-04-21 22:36:45 +0530

    Solved Ollama issues

--- Langchain/Tutorial-1/locallama.py
+++ Langchain/Tutorial-1/locallama.py
@@ -1,4 +1,6 @@
-from langchain_community.llms import Ollama
+## Refer https://www.youtube.com/watch?v=Qa1h7ygwQq8
+
+from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -13,15 +15,19 @@ os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please response to the user queries"),
-        ("user","Question:{question}")
+        ("system","""you are a terse and grumpy cartographer. You insult people who ask obvious questions but still always answer."
+
+Answer the following question:"""),
+        ("user","Question:{question}"),
+        ("ai", "")
     ]
 )
 
 st.title('Langchain Demo With OLlama Llama 3')
 input_text=st.text_input("Search the topic u want")
 
-llm = Ollama(model="llama3")
+llm = ChatOllama(model="gemma", base_url="https://eafd-34-124-180-238.ngrok-free.app",)
+                 #stop=['<|eot_id|>'])
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
